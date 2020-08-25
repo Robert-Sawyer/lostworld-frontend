f776f0d370cc3edd9f2abbc89001c783ded59404
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-08-25 13:05:04 +0200

    Profil sidebar - style

--- src/components/Profile/ProfileSidebar/ProfileSidebar.js
+++ src/components/Profile/ProfileSidebar/ProfileSidebar.js
@@ -8,8 +8,16 @@ const profileSidebar = props => (
             <img src={ProfilePhoto}/>
         </div>
         <div className={classes.Motto}><span>"Bez ryzyka nie ma zabawy"</span></div>
-        <div className={classes.UserInfo}>Informacje o użytkowniku</div>
-        <div className={classes.Friends}>Box ze znajomymi</div>
+        <div className={classes.UserInfo}>
+            <p>Na <span>Lost World</span> od: 25.08.2020</p>
+            <p>Odwiedzonych krajów: xx</p>
+            <p>Odwiedzonych miejsc: xxx</p>
+            <p>Dodanych miejsc:xx</p>
+            <p>Liczba planów podróży: xx</p>
+        </div>
+        <div className={classes.Friends}>
+            <p>Moi znajomi: </p>
+        </div>
     </nav>
 );
 
